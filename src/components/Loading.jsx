7e7f16c1e151c12de0a53a866d8c27e188394889
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-10-28 17:25:17 +0600

    home deco project done

---
+++ src/components/Loading.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Loading = ({counter = 6}) => {
+    return (
+        <div className='grid xl:lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mx-auto gap-20'>
+            {Array.from({ length: counter }).map((_ , i) => (
+            <div key={i} className="flex w-80 flex-col gap-4">
+                <div className="skeleton h-32 w-full"></div>
+                <div className="skeleton h-4 w-28"></div>
+                <div className="skeleton h-4 w-full"></div>
+                <div className="skeleton h-4 w-full"></div>
+            </div>
+            ))}
+        </div>
+
+    );
+};
+
+export default Loading;
